d4122fa63fadab90d51a8460f9919a5a9f96f5c0
Ben Pig Chu <benpichu@gmail.com>
2015-08-01 13:14:36 +0800

    basic js
    need fix

---
+++ js/part.js
@@ -0,0 +1,156 @@
+/* global game */
+//parts of game.snake
+function Part(length,startPoint,startDirection,partType) {
+	
+	if (partType!=-1&&partType!=1&&partType!=0) {
+		partType=0
+	}
+	
+	this.length=length
+	this.partType=partType //0=straight 1=clockwise -1=anticlockwise
+	this.startPoint=startPoint
+	this.startDirection=startDirection//0=right pi/2=up
+	
+	switch (this.partType) {
+		case 1:
+			this.arcCenter={
+				x:this.startPoint.x+game.snake.turningRadius*Math.sin(this.startDirection),
+				y:this.startPoint.y+game.snake.turningRadius*Math.cos(this.startDirection),
+			}
+			this.endDirection=this.startDirection-this.length/game.snake.turningRadius
+			if (this.endDirection<0) {this.endDirection+=2*Math.PI}
+			this.endPoint={
+				x:this.arcCenter.x-game.snake.turningRadius*Math.sin(this.endDirection),
+				y:this.arcCenter.y-game.snake.turningRadius*Math.cos(this.endDirection),
+			}
+			break;
+		case -1:
+			this.arcCenter={
+				x:this.startPoint.x-game.snake.turningRadius*Math.sin(this.startDirection),
+				y:this.startPoint.y-game.snake.turningRadius*Math.cos(this.startDirection),
+			}
+			this.endDirection=this.startDirection+this.length/game.snake.turningRadius
+			if (this.endDirection>2*Math.PI) {this.endDirection-=2*Math.PI}
+			this.endPoint={
+				x:this.arcCenter.x+game.snake.turningRadius*Math.sin(this.endDirection),
+				y:this.arcCenter.y+game.snake.turningRadius*Math.cos(this.endDirection),
+			}
+			break;
+		default://partType==0
+			this.endDirection=this.startDirection
+			this.endPoint={
+				x:this.startPoint.x+this.length*Math.cos(this.startDirection),
+				y:this.startPoint.y-this.length*Math.sin(this.startDirection),
+			}
+			this.arcCenter=null
+			break;
+	}
+	
+}
+
+Part.prototype.extend=function(extendLength) {
+	
+	this.length+=extendLength
+	
+	switch (this.partType) {
+		case 1:
+			this.endDirection=this.startDirection-this.length/game.snake.turningRadius
+			if (this.endDirection<0) {this.endDirection+=2*Math.PI}
+			this.endPoint={
+				x:this.arcCenter.x-game.snake.turningRadius*Math.sin(this.endDirection),
+				y:this.arcCenter.y-game.snake.turningRadius*Math.cos(this.endDirection),
+			}
+			break;
+		case -1:
+			this.endDirection=this.startDirection+this.length/game.snake.turningRadius
+			if (this.endDirection>2*Math.PI) {this.endDirection-=2*Math.PI}
+			this.endPoint={
+				x:this.arcCenter.x+game.snake.turningRadius*Math.sin(this.endDirection),
+				y:this.arcCenter.y+game.snake.turningRadius*Math.cos(this.endDirection),
+			}
+			break;
+		default://partType==0
+			this.endPoint={
+				x:this.startPoint.x+this.length*Math.cos(this.startDirection),
+				y:this.startPoint.y-this.length*Math.sin(this.startDirection),
+			}
+			break;
+	}
+	
+}
+
+Part.prototype.shorten=function(shortenLength) {
+	
+	this.length-=shortenLength
+	
+	switch (this.partType) {
+		case 1:
+			this.startDirection=this.endDirection+this.length/game.snake.turningRadius
+			if (this.endDirection<0) {this.endDirection+=2*Math.PI}
+			this.startPoint={
+				x:this.arcCenter.x-game.snake.turningRadius*Math.sin(this.startDirection),
+				y:this.arcCenter.y-game.snake.turningRadius*Math.cos(this.startDirection),
+			}
+			break;
+		case -1:
+			this.startDirection=this.endDirection-this.length/game.snake.turningRadius
+			if (this.endDirection>2*Math.PI) {this.endDirection-=2*Math.PI}
+			this.startPoint={
+				x:this.arcCenter.x+game.snake.turningRadius*Math.sin(this.staerDirection),
+				y:this.arcCenter.y+game.snake.turningRadius*Math.cos(this.startDirection),
+			}
+			break;
+		default://partType==0
+			this.startPoint={
+				x:this.endPoint.x-this.length*Math.cos(this.startDirection),
+				y:this.endPoint.y+this.length*Math.sin(this.startDirection),
+			}
+			break;
+	}
+	if (this.startPoint.x<0) {
+		this.startPoint.x+=game.groundSize
+		this.endPoint.x+=game.groundSize
+	} else if (this.startPoint.x>=game.groundSize) {
+		this.startPoint.x-=game.groundSize
+		this.endPoint.x-=game.groundSize
+	} 
+	if (this.startPoint.y<0) {
+		this.startPoint.y+=game.groundSize
+		this.endPoint.y+=game.groundSize
+	} else if (this.startPoint.y>=game.groundSize) {
+		this.startPoint.y-=game.groundSize
+		this.endPoint.y-=game.groundSize
+	}
+	
+}
+
+Part.prototype.draw=function() {
+	
+	game.canvas.beginPath()
+	for (var i=-1;i<2;i++) {
+		for (var j=-1;j<2;j++) {
+			switch (this.partType) {
+			case 1:
+				game.canvas.arc(this.arcCenter.x+i*game.groundSize,this.arcCenter.y+j*game.groundSize,game.snake.turningRadius+game.snake.width/2,this.startDirection+Math.PI/2,this.endDirection+Math.PI/2,false)
+				game.canvas.arc(this.arcCenter.x+i*game.groundSize,this.arcCenter.y+j*game.groundSize,game.snake.turningRadius-game.snake.width/2,this.endDirection+Math.PI/2,this.startDirection+Math.PI/2,true)
+				break;
+			case -1:
+				game.canvas.arc(this.arcCenter.x+i*game.groundSize,this.arcCenter.y+j*game.groundSize,game.snake.turningRadius+game.snake.width/2,this.startDirection-Math.PI/2,this.endDirection-Math.PI/2,true)
+				game.canvas.arc(this.arcCenter.x+i*game.groundSize,this.arcCenter.y+j*game.groundSize,game.snake.turningRadius-game.snake.width/2,this.endDirection-Math.PI/2,this.startDirection-Math.PI/2,false)
+				break;
+			default://partType==0
+				game.canvas.moveTo(this.startPoint.x+Math.sin(this.startDirection)*(game.snake.width/2)+i*game.groundSize,this.startPoint.y+Math.cos(this.startDirection)*(game.snake.width/2)+j*game.groundSize)
+				game.canvas.lineTo(this.startPoint.x-Math.sin(this.startDirection)*(game.snake.width/2)+i*game.groundSize,this.startPoint.y-Math.cos(this.startDirection)*(game.snake.width/2)+j*game.groundSize)
+				game.canvas.lineTo(this.endPoint.x-Math.sin(this.startDirection)*(game.snake.width/2)+i*game.groundSize,this.endPoint.y-Math.cos(this.startDirection)*(game.snake.width/2)+j*game.groundSize)
+				game.canvas.lineTo(this.endPoint.x+Math.sin(this.startDirection)*(game.snake.width/2)+i*game.groundSize,this.endPoint.y+Math.cos(this.startDirection)*(game.snake.width/2)+j*game.groundSize)
+				break;
+			}
+			game.canvas.fillStyle=game.color
+			game.canvas.fill()
+			game.canvas.closePath()
+			game.canvas.beginPath()
+			
+		}
+	}
+	
+}
---
+++ js/snake.js
@@ -0,0 +1,91 @@
+//snake
+function Snake(length,width,speed,turningRadius) {
+	
+	this.length=length
+	this.width=width
+	this.speed=speed
+	this.turningRadius=turningRadius
+	this.parts=[new Part(this.length,{x:game.groundSize/2,y:game.groundSize/2+this.width/2+this.length/2},Math.PI/2,0)]
+	this.headPoint={x:game.groundSize/2,y:game.groundSize/2-this.width/2-this.length/2}
+	this.ungrownLength=0
+	
+}
+
+Snake.prototype.extend=function(length,extendType) {
+	
+	if (extendType!=this.parts[this.parts.length-1].partType||length+this.parts[this.parts.length-1].length>game.groundSize-this.width) {
+		var newStartPoint=this.parts[this.parts.length-1].endPoint
+		if (newStartPoint.x<0) {
+			newStartPoint.x+=game.groundSize
+		} else if (newStartPoint.x>=game.groundSize) {
+			newStartPoint.x-=game.groundSize
+		} 
+		if (newStartPoint.y<0) {
+			newStartPoint.y+=game.groundSize
+		} else if (newStartPoint.y>=game.groundSize) {
+			newStartPoint.y-=game.groundSize
+		}
+		this.parts.push(new Part(length,newStartPoint,this.parts[this.parts.length-1].endDirection,extendType))
+	} else {
+		this.parts[this.parts.length-1].extend(length)
+	}
+	this.headPoint={
+		x:this.parts[this.parts.length-1].endPoint.x+this.width*Math.cos(this.parts[this.parts.length-1].endDirection),
+		y:this.parts[this.parts.length-1].endPoint.y-this.width*Math.sin(this.parts[this.parts.length-1].endDirection),
+	}
+	if (this.headPoint.x<0) {
+			this.headPoint.x+=game.groundSize
+		} else if (this.headPoint.x>=game.groundSize) {
+			this.headPoint.x-=game.groundSize
+		} 
+		if (this.headPoint.y<0) {
+			this.headPoint.y+=game.groundSize
+		} else if (this.headPoint.y>=game.groundSize) {
+			this.headPoint.y-=game.groundSize
+		}
+	
+}
+
+Snake.prototype.shorten=function(length) {
+	
+	while (this.parts[0].length<=length) {
+		length-=this.parts[0].length
+		this.parts.shift()
+	}
+	this.parts[0].shorten(length)
+	
+}
+
+Snake.prototype.move=function(moveType) {
+	
+	this.extend(this.speed,moveType)
+	if (this.speed<=this.ungrownLength) {
+		this.ungrownLength-=this.speed
+	} else {
+		this.shorten(this.speed-this.ungrownLength)
+		this.ungrownLength=0
+	}
+	
+}
+
+Snake.prototype.draw=function() {
+	
+	for (var t = 0; t < this.parts.length; t++) {
+		this.parts[t].draw()
+	}
+	var headDirection=this.parts[this.parts.length-1].endDirection
+	for (var i=-1;i<2;i++) {
+		for (var j=-1;j<2;j++) {
+			
+			game.canvas.moveTo(this.headPoint.x+i*game.groundSize,this.headPoint.y+j*game.groundSize)
+			game.canvas.lineTo(this.headPoint.x-this.width*Math.cos(headDirection)+(this.width/2)*Math.sin(headDirection)+i*game.groundSize,this.headPoint.y+this.width*Math.sin(headDirection)+(this.width/2)*Math.cos(headDirection)+j*game.groundSize)
+			game.canvas.lineTo(this.headPoint.x-this.width*Math.cos(headDirection)-(this.width/2)*Math.sin(headDirection)+i*game.groundSize,this.headPoint.y+this.width*Math.sin(headDirection)-(this.width/2)*Math.cos(headDirection)+j*game.groundSize)
+			game.canvas.fillStyle=game.color
+			game.canvas.fill()
+			game.canvas.closePath()
+			game.canvas.beginPath()
+			
+		}
+	}
+	
+}
