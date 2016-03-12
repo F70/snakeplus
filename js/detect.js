152e264fe323f05974cc5f1e7f2c505987a86c2e
Ben Pig Chu <benpichu@gmail.com>
2016-03-12 13:06:42 +0800

    detect canvas, transition and animation support

--- js/detect.js
+++ js/detect.js
@@ -1,3 +1,14 @@
+var systemVar={}
+
+try{
+	if(!("createPattern" in document.createElement("canvas").getContext("2d"))){throw(new ReferenceError())}
+	if(!("transition" in document.createElement("div").style)||!("animation" in document.createElement("div").style)){throw(new ReferenceError())}
+}catch(e){
+	requestAnimationFrame(function(){
+		document.getElementById("error").style.display="block"
+	})
+}
+
 systemVar.isTouch=("ontouchmove" in document)
 if(systemVar.isTouch){document.getElementsByTagName("html")[0].classList.add("touch")}
 document.getElementById("viewport").setAttribute("content","width=100,user-scalable=no")
